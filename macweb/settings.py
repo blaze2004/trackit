d3a6d07367d9b6195dc2601a18540ffcdc850400
Shubham Tiwari <72434294+blaze2004@users.noreply.github.com>
2022-01-08 12:34:46 +0530

    Add files via upload

--- macweb/settings.py
+++ macweb/settings.py
@@ -23,10 +23,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-5#+&!ovhgjn2xmiw4ys2ko!iq6llhf)jt$2vjz4ic+un$amh9x'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
