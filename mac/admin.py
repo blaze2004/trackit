0cd30da92cd6f7923a0526add5bb4dc77d636f54
Shubham Tiwari <72434294+blaze2004@users.noreply.github.com>
2022-01-08 12:14:06 +0530

    First commit

--- mac/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from django.db.models.base import Model
-
-# Register your models here.
-from .models import Features
-
-admin.site.register(Features)
--- static/js/base_generic.js
+++
@@ -1,5 +0,0 @@
-window.addEventListener('scroll', function () {
-    let header = document.querySelector('header');
-    let windowlPosition = window.scrollY > 0;
-    header.classList.toggle('scrolling-active', windowlPosition);
-});
