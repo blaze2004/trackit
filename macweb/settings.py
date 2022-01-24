615335db8106d08c939190c7aa830ac2ada8f783
blaze2004 <shubhamtiwari06112004@gmail.com>
2022-01-24 23:19:41 +0530

    updated requirements.txt

--- macweb/settings.py
+++ macweb/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['https://meetattendance.herokuapp.com']
 
 
 # Application definition
