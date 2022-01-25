02ff9e93b4974a21d69a2ce40a8fb0a9d8f814cf
blaze2004 <shubhamtiwari06112004@gmail.com>
2022-01-25 22:09:36 +0530

    removed templates

--- macweb/settings.py
+++ macweb/settings.py
@@ -65,7 +65,7 @@ ROOT_URLCONF = 'macweb.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR,'templates')],
+        'DIRS': [],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
