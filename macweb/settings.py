3f9bbd74f45dd50e361d19a9115a84629f47f43c
blaze2004 <shubhamtiwari06112004@gmail.com>
2022-01-25 14:28:55 +0530

    updated database

--- macweb/settings.py
+++ macweb/settings.py
@@ -66,7 +66,7 @@ ROOT_URLCONF = 'macweb.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR,'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -87,8 +87,12 @@ WSGI_APPLICATION = 'macweb.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'meet_attendance',
+        'USER': 'blaze@meetattendance',
+        'PASSWORD': 'Mac@django',
+        'HOST': 'meetattendance.mysql.database.azure.com',
+        'PORT': '3306',
     }
 }
 
