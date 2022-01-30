68abfc172defabf6149c07ce55b0e93883ed4e6a
blaze2004 <shubhamtiwari06112004@gmail.com>
2022-01-30 22:32:58 +0530

    updated homepage added firefox extension link

--- macweb/settings.py
+++ macweb/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['http://localhost','http://127.0.0.1', 'https://trackitnow.pythonanywhere.com']
+ALLOWED_HOSTS = ['localhost','127.0.0.1', 'https://trackitnow.pythonanywhere.com']
 
 
 # Application definition
