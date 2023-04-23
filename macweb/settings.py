f27156c4d6924d4b74a17ebad4cad666d835ba80
Shubham Tiwari <72434294+blaze2004@users.noreply.github.com>
2023-04-23 18:04:28 +0000

    updated staticfiles path

--- macweb/settings.py
+++ macweb/settings.py
@@ -140,7 +140,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'app/staticfiles')
 
 STATIC_URL = 'static/'
 
