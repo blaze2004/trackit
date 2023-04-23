63f73348f57a3dcc1743e2914f351d413a23a4f6
Shubham Tiwari <72434294+blaze2004@users.noreply.github.com>
2023-04-23 18:17:51 +0000

    updated static root

--- macweb/settings.py
+++ macweb/settings.py
@@ -140,9 +140,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'app/staticfiles')
+STATIC_ROOT = '/app/staticfiles'
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
