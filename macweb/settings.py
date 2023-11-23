1bc21a058618de4ca75dafca169c4dd8d4208250
Shubham Tiwari <72434294+blaze2004@users.noreply.github.com>
2023-11-23 22:35:14 +0530

    added allauth middleware

--- macweb/settings.py
+++ macweb/settings.py
@@ -68,6 +68,7 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     "whitenoise.middleware.WhiteNoiseMiddleware",
+    "allauth.account.middleware.AccountMiddleware",
 ]
 
 ROOT_URLCONF = 'macweb.urls'
